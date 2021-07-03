6a60d524996efbabacb49abdf1cf58006f9e31e2
mokoshin0720 <shinya.work0720@gmail.com>
2021-07-03 13:29:03 +0900

    [add]ps command

--- system_programming/xv6-env/xv6-public/ps.c
+++ system_programming/xv6-env/xv6-public/ps.c
@@ -7,6 +7,7 @@
 int
 main(void)
 {
+  // procを列挙
   enum procstate {UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE};
   
   // 静的配列の定義
@@ -19,16 +20,20 @@ main(void)
     [ZOMBIE]        "ZOMBIE",
   };
 
+  // プロセス情報が書かれているprocinfo型を定義
   struct procinfo procinfo_table[NPROC];
+  // getprocsで動いているプロセス数の取得
   int numbers = getprocs(procinfo_table);
 
+  printf(1, "pid / ppid / state / name / sz\n");
   for (int i = 0; i < numbers; i++) {
+    // 使われていないプロセスは飛ばす
     if (procinfo_table[i].state == UNUSED)
         continue;
 
-    printf(1, "%d %d", procinfo_table[i].pid, procinfo_table[i].ppid);
-    printf(1, " %s %s", states[procinfo_table[i].state], procinfo_table[i].name); 
-    printf(1, " %d", procinfo_table[i].sz);
+    printf(1, "%d / %d / ", procinfo_table[i].pid, procinfo_table[i].ppid);
+    printf(1, "%s / %s / ", states[procinfo_table[i].state], procinfo_table[i].name); 
+    printf(1, "%d / ", procinfo_table[i].sz);
     printf(1, "\n");
   }
 
