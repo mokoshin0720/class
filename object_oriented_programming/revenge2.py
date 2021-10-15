da181547e2999fe1d88e34febe13077732444e9a
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-15 21:04:59 +0900

    [add]object_oriented part1

---
+++ object_oriented_programming/revenge2.py
@@ -0,0 +1,15 @@
+def question2():
+    ans = 12345678 ** 9
+    return ans
+
+def question3():
+    ans = "ABC" * 12
+    return ans
+
+def question4():
+    a = 4+3j
+    b = 2-7j
+    return a*b
+
+if __name__ == "__main__":
+    print(question4())
