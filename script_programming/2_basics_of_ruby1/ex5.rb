31c33755489dcec2ba48471840520e378ff326c7
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-21 15:38:17 +0900

    [add]script_programming part2

---
+++ script_programming/2_basics_of_ruby1/ex5.rb
@@ -0,0 +1,15 @@
+dictionary = { 'dog' => '犬', 'cat' => '猫' }
+
+loop do
+    print '英語：'
+    x = gets.chomp
+    if dictionary[x]
+        puts "日本語：#{dictionary[x]}"
+    else
+        print "#{x}の日本語訳を教えてください。:"
+        ans = gets.chomp
+        dictionary[x] = ans
+        puts "英語：#{x}"
+        puts "日本語：#{dictionary[x]}"
+    end
+end
