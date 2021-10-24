e85bae82a86e85addf1035ad86ca651cb00549e0
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-24 19:58:58 +0900

    [add]object 2

---
+++ object_oriented_programming/revenge3.py
@@ -0,0 +1,15 @@
+def question2():
+    ans = ("Jean Du Breuil", "Diverses methodes universelles et nouvelles", 1642)
+    print(ans)
+
+def question3(x):
+    print(x%100)
+
+def question4(x):
+    print(x.imag)
+
+def question5(s):
+    ans = ascii(s)
+    print(ans)
+
+question5("蛸")
