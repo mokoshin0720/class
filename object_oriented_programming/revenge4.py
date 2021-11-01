6b4ab9ba1a0a7cfa13ed0d9c861ffb00c15d85aa
mokoshin0720 <shinya.work0720@gmail.com>
2021-11-01 13:25:03 +0900

    [add]object_oriented ex4

---
+++ object_oriented_programming/revenge4.py
@@ -0,0 +1,17 @@
+def question2():
+    s = "aiueokakikukeko"
+    print(f"{s[:5]}")
+
+def question3():
+    latitude = -1234567.777777
+    print(f"{latitude:.3f}")
+
+def question4():
+    binary = 12
+    print(f"{binary:08b}")
+
+def question5():
+    seven = 123
+    print(f"{seven//7//7%7}{seven//7%7}{seven%7}")
+
+question3()
