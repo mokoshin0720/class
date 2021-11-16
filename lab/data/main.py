bbf263d84a8a1e6cd284de9148ae071480949182
mokoshin0720 <shinya.work0720@gmail.com>
2021-11-17 02:01:19 +0900

    正規表現の追加

--- lab/data/main.py
+++ lab/data/main.py
@@ -1,15 +1,30 @@
 import tweepy
 import config
+import demoji
+import re
 
 def run():
     api = config.get_api()
 
-    for tweet in tweepy.Cursor(api.search_tweets, q="スマブラ").items(10):
-        if tweet.in_reply_to_user_id == None:
-            continue
+    for tweet in tweepy.Cursor(api.search_tweets, q="a -filter:retweets").items(10):
+        # if tweet.in_reply_to_user_id == None:
+        #     continue
         print(tweet.text)
-        print(tweet.id)
-        print(tweet.user.id)
+
+        text = clean_text(tweet.text)
+        print(text)
+        print(check_text(text))
+        print("======================")
+
+def clean_text(text):
+    text = demoji.replace(string=text)
+    text = text.replace("@", "to")
+    text = re.sub(r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+)", "" , text)
+    return text
+
+def check_text(text):
+    p = re.compile(r'^[a-zA-Z0-9\?!\$&\-(),…\.\' ]+$')
+    return p.fullmatch(text)
 
 if __name__ == "__main__":
     run()
