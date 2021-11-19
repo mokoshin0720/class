b818c84fae0b85f9f301c7dffed8e15923507667
mokoshin0720 <shinya.work0720@gmail.com>
2021-11-20 00:50:29 +0900

    目的の会話は取得した

--- lab/main.py
+++ lab/main.py
@@ -11,25 +11,29 @@ def run():
     q = "? -filter:retweets exclude:retweets lang:en"
     for tweet in tweepy.Cursor(api.search_tweets, q=q, result_type="mixed", tweet_mode='extended').items(100):
         conversation_list = []
+        conversation_valid_flg = False
 
-        if is_valid_tweet(tweet) == False:
+        text, ok = is_valid_tweet(tweet)
+        if ok == False:
             continue
 
-        text = clean_text(tweet.full_text)
         conversation_list.append(text)
 
-        print(text)
-        print(get_tweet_url(tweet))
         print("**********************")
         print("** New Conversation **")
         print("**********************")
+        print(get_tweet_url(tweet))
 
         turn = 0
         context = []
         while tweet.in_reply_to_status_id != None:
             try:
-                if tweet.lang != "en" or turn >= 5:
+                if turn >= 5:
                     break
+                if tweet.lang != "en":
+                    conversation_valid_flg = True
+                    break
+
                 turn += 1
                 tweet = searcy_by_id(tweet.in_reply_to_status_id)
                 text = clean_text(tweet.full_text)
@@ -38,10 +42,12 @@ def run():
             except Exception as e:
                 break
 
-            print(turn)
-            print(text)
-            print("====================")
-        
+        if conversation_valid_flg:
+            print("**********************")
+            print("** 外側から抜ける **")
+            print("**********************")
+            continue
+
         conversation_list.append(context)
         all_list.append(conversation_list)
     print(all_list)
@@ -57,22 +63,31 @@ def clean_text(text):
     text = re.sub(r"@(\w+)", "", text) # @メンション削除
     text = re.sub(r"#(\w+)", "", text) # ハッシュタグ削除
     text = re.sub(r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+)", "" , text) # url削除
+    text = text.replace("\n", "")
+    text = text.replace("\t", "")
     return text
 
-def is_valid_tweet(tweet):
+def is_valid_tweet(tweet) -> (str, bool):
     if tweet.in_reply_to_user_id == None:
-        return False
+        return "", False
     
     if tweet.metadata["iso_language_code"] != "en":
-        return False
+        return "", False
 
-    return True
+    text = clean_text(tweet.full_text)
+
+    pattern = re.compile(r"\?$")
+    if bool(pattern.search(text)) == False:
+        return "", False
+
+    return text, True
 
 def get_tweet_url(tweet):
     user_id = tweet.user.id_str
     tweet_id = tweet.id_str
     url = "https://twitter.com/" + user_id + "/status/" + tweet_id
     return url
+
 
 if __name__ == "__main__":
     run()
