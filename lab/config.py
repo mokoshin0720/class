30a7c5b3f2c8a5646d7e4466547edce629d7f621
mokoshin0720 <shinya.work0720@gmail.com>
2021-11-21 14:46:44 +0900

    現在のcntを可視化

--- lab/config.py
+++ lab/config.py
@@ -13,6 +13,6 @@ def get_api():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
-    api = tweepy.API(auth)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
     return api
